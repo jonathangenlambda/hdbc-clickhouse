53464422fefdf67307f8d8dc91862e0525a01702
Jonathan <jonathan@genlambdad.com>
2023-04-25 18:03:48 +0200

    added readDateTime64

--- src/Database/HDBC/ClickHouse/Codec/Decoder.hs
+++ src/Database/HDBC/ClickHouse/Codec/Decoder.hs
@@ -1,67 +1,73 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Database.HDBC.ClickHouse.Codec.Decoder where
 
 import Control.Exception (throwIO)
-import Data.Array.ST (newArray, readArray, MArray, STUArray)
+import Data.Array.ST (MArray, STUArray, newArray, readArray)
 import Data.Array.Unsafe (castSTUArray)
 import Data.Bits (Bits, shiftL, (.&.), (.|.))
-import Data.Int (Int8, Int16, Int32, Int64)
+import Data.Int (Int16, Int32, Int64, Int8)
 import Data.List ((\\))
 import Data.Time (UTCTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
-import Data.Word (Word8, Word16, Word32, Word64)
-import Database.HDBC.ClickHouse.Exception (ClickHouseException(..), unexpectedResponse)
-import GHC.ST (runST, ST)
+import Data.Word (Word16, Word32, Word64, Word8)
+import Database.HDBC.ClickHouse.Exception (ClickHouseException (..), unexpectedResponse)
+import GHC.ST (ST, runST)
 import Network.Socket (Socket)
 import Network.Socket.ByteString (recv)
 
 import qualified Codec.Binary.UTF8.String as C
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
+import Data.Time.Clock.System
 
 readString :: Socket -> IO String
 readString sock = do
-  size <- readNum sock
-  bs <- if size == 0
-    then return B.empty
-    else recv sock $ fromIntegral size
-  return $ C.decodeString $ B8.unpack bs
+    size <- readNum sock
+    bs <-
+        if size == 0
+            then return B.empty
+            else recv sock $ fromIntegral size
+    return $ C.decodeString $ B8.unpack bs
 
 readFixedString :: Socket -> Int -> IO String
 readFixedString sock size = do
-  bs <- if size == 0
-    then return B.empty
-    else recv sock size
-  return $ C.decodeString $ B8.unpack $ B.map replaceNULToEmptyChar bs
-    where
-      replaceNULToEmptyChar 0 = 32
-      replaceNULToEmptyChar x = x
+    bs <-
+        if size == 0
+            then return B.empty
+            else recv sock size
+    return $ C.decodeString $ B8.unpack $ B.map replaceNULToEmptyChar bs
+  where
+    replaceNULToEmptyChar 0 = 32
+    replaceNULToEmptyChar x = x
 
 readNum :: Socket -> IO Word64
 readNum sock = readNum' sock 0 0
   where
     readNum' :: Socket -> Word64 -> Int -> IO Word64
     readNum' sock n s = do
-      bs <- recv sock 1
-      case (B.unpack bs) of
-        [b] | b < 0x80  -> return $ fromIntegral (n .|. (fromIntegral b) `shiftL` s)
-            | otherwise -> readNum' sock (fromIntegral (n .|. ((fromIntegral b) .&. 0x7f) `shiftL` s)) (s + 7)
-        xs -> throwIO $ unexpectedResponse "Number" xs
+        bs <- recv sock 1
+        case (B.unpack bs) of
+            [b]
+                | b < 0x80 -> return $ fromIntegral (n .|. (fromIntegral b) `shiftL` s)
+                | otherwise -> readNum' sock (fromIntegral (n .|. ((fromIntegral b) .&. 0x7f) `shiftL` s)) (s + 7)
+            xs -> throwIO $ unexpectedResponse "Number" xs
 
 readBool :: Socket -> IO Bool
 readBool sock = do
-  bs <- recv sock 1
-  case (B.unpack bs) of
-    [1] -> return True
-    [0] -> return False
-    xs  -> throwIO $ unexpectedResponse "Bool" xs
+    bs <- recv sock 1
+    case (B.unpack bs) of
+        [1] -> return True
+        [0] -> return False
+        xs -> throwIO $ unexpectedResponse "Bool" xs
 
 readInt8 :: Socket -> IO Int8
 readInt8 sock = do
-  bs <- recv sock 1
-  case (B.unpack bs) of
-    [n] -> return $ fromIntegral n
-    xs  -> throwIO $ unexpectedResponse "Int8" xs
+    bs <- recv sock 1
+    case (B.unpack bs) of
+        [n] -> return $ fromIntegral n
+        xs -> throwIO $ unexpectedResponse "Int8" xs
 
 readInt16 :: Socket -> IO Int16
 readInt16 sock = readFixedNum sock 0 2
@@ -74,10 +80,10 @@ readInt64 sock = readFixedNum sock 0 8
 
 readWord8 :: Socket -> IO Word8
 readWord8 sock = do
-  bs <- recv sock 1
-  case (B.unpack bs) of
-    [n] -> return n
-    xs  -> throwIO $ unexpectedResponse "Word8" xs
+    bs <- recv sock 1
+    case (B.unpack bs) of
+        [n] -> return n
+        xs -> throwIO $ unexpectedResponse "Word8" xs
 
 readWord16 :: Socket -> IO Word16
 readWord16 sock = readFixedNum sock 0 2
@@ -90,19 +96,19 @@ readWord64 sock = readFixedNum sock 0 8
 
 readFixedNum :: (Num a, Bits a) => Socket -> a -> Int -> IO a
 readFixedNum sock zero size = do
-  bs <- recv sock size
-  let (i, _) =  B.foldl (\(n, s) b -> (n .|. ((fromIntegral b) `shiftL` s), s + 8)) (zero, 0) bs
-  return i
+    bs <- recv sock size
+    let (i, _) = B.foldl (\(n, s) b -> (n .|. ((fromIntegral b) `shiftL` s), s + 8)) (zero, 0) bs
+    return i
 
 readFloat32 :: Socket -> IO Float
 readFloat32 sock = do
-  w32 <- readWord32 sock
-  return $ wordToFloat w32
+    w32 <- readWord32 sock
+    return $ wordToFloat w32
 
 readFloat64 :: Socket -> IO Double
 readFloat64 sock = do
-  w64 <- readWord64 sock
-  return $ wordToDouble w64
+    w64 <- readWord64 sock
+    return $ wordToDouble w64
 
 -- https://stackoverflow.com/a/7002812
 wordToFloat :: Word32 -> Float
@@ -112,44 +118,58 @@ wordToDouble :: Word64 -> Double
 wordToDouble x = runST (cast x)
 
 {-# INLINE cast #-}
-cast :: (MArray (STUArray s) a (ST s),
-         MArray (STUArray s) b (ST s)) => a -> ST s b
+cast ::
+    ( MArray (STUArray s) a (ST s)
+    , MArray (STUArray s) b (ST s)
+    ) =>
+    a ->
+    ST s b
 cast x = newArray (0 :: Int, 0) x >>= castSTUArray >>= flip readArray 0
 
 readDate :: Socket -> IO UTCTime
 readDate sock = do
-  sec <- readInt16 sock
-  return $ posixSecondsToUTCTime $ (fromIntegral sec) * 24 * 3600
+    sec <- readInt16 sock
+    return $ posixSecondsToUTCTime $ (fromIntegral sec) * 24 * 3600
 
 readDateTime :: Socket -> IO UTCTime
 readDateTime sock = do
-  sec <- readInt32 sock
-  return $ posixSecondsToUTCTime $ fromIntegral sec
+    sec <- readInt32 sock
+    return $ posixSecondsToUTCTime $ fromIntegral sec
+
+readDateTime64 :: Socket -> IO UTCTime
+readDateTime64 sock = mkUtcTime <$> readInt64 sock
+  where
+    mkUtcTime :: Int64 -> UTCTime
+    mkUtcTime nanosEpoch = systemToUTCTime $ MkSystemTime seconds nanos
+      where
+        (seconds, fromIntegral -> nanos) = nanosEpoch `divMod` (10 ^ 9)
 
 readException :: Socket -> IO ClickHouseException
 readException sock = do
-  code <- readInt32 sock
-  name <- readString sock
-  message <- readString sock
-  stackTrace <- readString sock
-  hasNested <- readBool sock
-  nested <- if hasNested
-    then fmap Just (readException sock)
-    else return Nothing
-  return ServerException {
-    code = fromIntegral code,
-    name = name,
-    message = message \\ (name ++ ": "),
-    stackTrace = stackTrace,
-    nested = nested
-  }
+    code <- readInt32 sock
+    name <- readString sock
+    message <- readString sock
+    stackTrace <- readString sock
+    hasNested <- readBool sock
+    nested <-
+        if hasNested
+            then fmap Just (readException sock)
+            else return Nothing
+    return
+        ServerException
+            { code = fromIntegral code
+            , name = name
+            , message = message \\ (name ++ ": ")
+            , stackTrace = stackTrace
+            , nested = nested
+            }
 
 readAll :: Socket -> IO B.ByteString
 readAll sock =
-  readAll' sock B.empty
+    readAll' sock B.empty
   where
     readAll' sock bs = do
-      r <- recv sock 1024
-      if (B.length r == 0 || B.last r == 4)
-        then return $ bs `B.append` r
-        else readAll' sock $ bs `B.append` r
+        r <- recv sock 1024
+        if (B.length r == 0 || B.last r == 4)
+            then return $ bs `B.append` r
+            else readAll' sock $ bs `B.append` r
--- src/Database/HDBC/ClickHouse/Data/Reader.hs
+++ src/Database/HDBC/ClickHouse/Data/Reader.hs
@@ -4,9 +4,9 @@ import Data.Bits
 import Data.IP (toIPv4, toIPv6)
 import Data.List (intersperse, unfoldr)
 import Data.Word
-import Database.HDBC.SqlValue
 import Database.HDBC.ClickHouse.Data.Column
-import Database.HDBC.ClickHouse.Protocol (joinSqlValues, Config)
+import Database.HDBC.ClickHouse.Protocol (Config, joinSqlValues)
+import Database.HDBC.SqlValue
 import Network.Socket (Socket)
 import Numeric (showHex)
 
@@ -14,80 +14,82 @@ import qualified Database.HDBC.ClickHouse.Codec.Decoder as D
 
 readValue :: Socket -> Column -> Config -> Int -> IO [SqlValue]
 readValue sock (StringColumn _) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readString sock
+    readEachValue numRows $ fmap toSql $ D.readString sock
 readValue sock (Int8Column _) _ numRows =
-  readEachValue numRows $ fmap (iToSql . fromIntegral) $ D.readInt8 sock
+    readEachValue numRows $ fmap (iToSql . fromIntegral) $ D.readInt8 sock
 readValue sock (Int16Column _) _ numRows =
-  readEachValue numRows $ fmap (iToSql . fromIntegral) $ D.readInt16 sock
+    readEachValue numRows $ fmap (iToSql . fromIntegral) $ D.readInt16 sock
 readValue sock (Int32Column _) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readInt32 sock
+    readEachValue numRows $ fmap toSql $ D.readInt32 sock
 readValue sock (Int64Column _) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readInt64 sock
+    readEachValue numRows $ fmap toSql $ D.readInt64 sock
 readValue sock (UInt8Column _) _ numRows =
-  readEachValue numRows $ fmap (toSql . toWord32) $ D.readWord8 sock
+    readEachValue numRows $ fmap (toSql . toWord32) $ D.readWord8 sock
 readValue sock (UInt16Column _) _ numRows =
-  readEachValue numRows $ fmap (toSql . toWord32) $ D.readWord16 sock
+    readEachValue numRows $ fmap (toSql . toWord32) $ D.readWord16 sock
 readValue sock (UInt32Column _) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readWord32 sock
+    readEachValue numRows $ fmap toSql $ D.readWord32 sock
 readValue sock (UInt64Column _) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readWord64 sock
+    readEachValue numRows $ fmap toSql $ D.readWord64 sock
 readValue sock (Float32Column _) _ numRows =
-  readEachValue numRows $ fmap (toSql . toDouble) $ D.readFloat32 sock
+    readEachValue numRows $ fmap (toSql . toDouble) $ D.readFloat32 sock
 readValue sock (Float64Column _) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readFloat64 sock
+    readEachValue numRows $ fmap toSql $ D.readFloat64 sock
 readValue sock (DateColumn _) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readDate sock -- TODO: timezone
+    readEachValue numRows $ fmap toSql $ D.readDate sock -- TODO: timezone
 readValue sock (DateTimeColumn _) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readDateTime sock -- TODO: timezone
+    readEachValue numRows $ fmap toSql $ D.readDateTime sock -- TODO: timezone
+readValue sock (DateTime64 _ _size) _ numRows =
+    readEachValue numRows $ fmap toSql $ D.readDateTime64 sock -- TODO: timezone
 readValue sock (FixedStringColumn _ size) _ numRows =
-  readEachValue numRows $ fmap toSql $ D.readFixedString sock size
+    readEachValue numRows $ fmap toSql $ D.readFixedString sock size
 readValue sock (UUIDColumn _) _ numRows =
-  readEachValue numRows read
-    where
-      read = do
-        bytes <- mapM (\_ -> fmap fromIntegral $ D.readWord8 sock) [1..16]
-        let uuids = foldl toUUID ("", "", "", "", "") $ zip bytes [0..]
+    readEachValue numRows read
+  where
+    read = do
+        bytes <- mapM (\_ -> fmap fromIntegral $ D.readWord8 sock) [1 .. 16]
+        let uuids = foldl toUUID ("", "", "", "", "") $ zip bytes [0 ..]
         return $ toSql $ concat $ intersperse "-" $ tapleTolist uuids
-      toUUID (uuid1, uuid2, uuid3, uuid4, uuid5) (byte, index)
-        | index >= 4  && index <= 7  = (toHex byte uuid1, uuid2, uuid3, uuid4, uuid5)
-        | index >= 2  && index <= 3  = (uuid1, toHex byte uuid2, uuid3, uuid4, uuid5)
-        | index >= 0  && index <= 1  = (uuid1, uuid2, toHex byte uuid3, uuid4, uuid5)
+    toUUID (uuid1, uuid2, uuid3, uuid4, uuid5) (byte, index)
+        | index >= 4 && index <= 7 = (toHex byte uuid1, uuid2, uuid3, uuid4, uuid5)
+        | index >= 2 && index <= 3 = (uuid1, toHex byte uuid2, uuid3, uuid4, uuid5)
+        | index >= 0 && index <= 1 = (uuid1, uuid2, toHex byte uuid3, uuid4, uuid5)
         | index >= 14 && index <= 15 = (uuid1, uuid2, uuid3, toHex byte uuid4, uuid5)
-        | index >= 8  && index <= 13 = (uuid1, uuid2, uuid3, uuid4, toHex byte uuid5)
-      toHex b s
-        | b <= 15   = '0' : (showHex b s)
+        | index >= 8 && index <= 13 = (uuid1, uuid2, uuid3, uuid4, toHex byte uuid5)
+    toHex b s
+        | b <= 15 = '0' : (showHex b s)
         | otherwise = showHex b s
-      tapleTolist (uuid1, uuid2, uuid3, uuid4, uuid5) = [uuid1, uuid2, uuid3, uuid4, uuid5]
+    tapleTolist (uuid1, uuid2, uuid3, uuid4, uuid5) = [uuid1, uuid2, uuid3, uuid4, uuid5]
 readValue sock (IPv4Column _) _ numRows =
-  readEachValue numRows read
-    where
-      read = do
-        bytes <- mapM (\_ -> fmap fromIntegral $ D.readWord8 sock) [1..4]
+    readEachValue numRows read
+  where
+    read = do
+        bytes <- mapM (\_ -> fmap fromIntegral $ D.readWord8 sock) [1 .. 4]
         let ip = toIPv4 $ reverse bytes
         return $ toSql $ show ip
 readValue sock (IPv6Column _) _ numRows =
-  readEachValue numRows read
-    where
-      read = do
-        bytes <- mapM (\_ -> fmap fromIntegral $ D.readWord8 sock) [1..16]
+    readEachValue numRows read
+  where
+    read = do
+        bytes <- mapM (\_ -> fmap fromIntegral $ D.readWord8 sock) [1 .. 16]
         let ip = toIPv6 $ unfoldr f bytes
         return $ toSql $ show ip
-      f (x:y:zs) = Just ((x `shiftL` 8) .|. y, zs)
-      f _        = Nothing
+    f (x : y : zs) = Just ((x `shiftL` 8) .|. y, zs)
+    f _ = Nothing
 readValue sock (ArrayColumn _ itemType) config numRows = do
-  offsetSum <- mapM (\_ -> fmap fromIntegral $ D.readWord64 sock) [1..numRows]
-  let offsets = zipWith (-) offsetSum $ (0:offsetSum)
-  fmap (map $ joinSqlValues config) $ mapM (\offset -> readValue sock itemType config offset) offsets
+    offsetSum <- mapM (\_ -> fmap fromIntegral $ D.readWord64 sock) [1 .. numRows]
+    let offsets = zipWith (-) offsetSum $ (0 : offsetSum)
+    fmap (map $ joinSqlValues config) $ mapM (\offset -> readValue sock itemType config offset) offsets
 readValue sock (NullableColumn _ itemType) config numRows = do
-  isNulls <- mapM (\_ -> D.readBool sock) [1..numRows]
-  values <- readValue sock itemType config numRows
-  return $ map f $ zip values isNulls
-    where
-      f (_, True) = SqlNull
-      f (v, _   ) = v
+    isNulls <- mapM (\_ -> D.readBool sock) [1 .. numRows]
+    values <- readValue sock itemType config numRows
+    return $ map f $ zip values isNulls
+  where
+    f (_, True) = SqlNull
+    f (v, _) = v
 
 readEachValue numRows f =
-  mapM (\_ -> f) [1..numRows]
+    mapM (\_ -> f) [1 .. numRows]
 
 toWord32 :: Integral a => a -> Word32
 toWord32 = fromIntegral
